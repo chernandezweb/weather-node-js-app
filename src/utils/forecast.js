a62e70e00b7545c622a7536a5fb44287fdbbdf7f
Carlos <chernandezweb@gmail.com>
2019-06-09 20:39:14 -0400

    env port fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
@@ -12,6 +12,6 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
-}
+};
 
-module.exports = forecast
+module.exports = forecast;
