f0bcf414c721290354932caa4ffcd08873b89c37
Carlos <chernandezweb@gmail.com>
2019-06-09 20:38:07 -0400

    env port fix

--- src/app.js
+++ src/app.js
@@ -1,37 +1,38 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
         name: 'Andrew Mead'
     })
-})
+});
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         name: 'Andrew Mead'
     })
-})
+});
 
 app.get('/help', (req, res) => {
     res.render('help', {
@@ -39,7 +40,7 @@ app.get('/help', (req, res) => {
         title: 'Help',
         name: 'Andrew Mead'
     })
-})
+});
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
@@ -65,7 +66,7 @@ app.get('/weather', (req, res) => {
             })
         })
     })
-})
+});
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
@@ -74,11 +75,11 @@ app.get('/products', (req, res) => {
         })
     }
 
-    console.log(req.query.search)
+    console.log(req.query.search);
     res.send({
         products: []
     })
-})
+});
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
@@ -86,7 +87,7 @@ app.get('/help/*', (req, res) => {
         name: 'Andrew Mead',
         errorMessage: 'Help article not found.'
     })
-})
+});
 
 app.get('*', (req, res) => {
     res.render('404', {
@@ -94,8 +95,8 @@ app.get('*', (req, res) => {
         name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
-})
+});
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.')
-})
+});
