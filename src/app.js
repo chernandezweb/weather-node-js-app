ae761dae16f2fd2ac60ea8420ba21f7a0db0bb35
Carlos Hernandez <chernandezweb@gmail.com>
2019-10-02 21:23:09 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 global.__basedir = __dirname;
 
+console.log(__basedir);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
